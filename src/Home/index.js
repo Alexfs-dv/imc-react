e8aee274eb72424b1780828d30409c9900ef9f3c
Alex <alexfaustinosouza@gmail.com>
2024-06-27 23:34:02 -0400

    commit finish

---
+++ src/Home/index.js
@@ -0,0 +1,69 @@
+import { useState } from 'react';
+import './home.css';
+
+
+export default function Home(){
+    const [peso, setPeso] = useState('');
+    const [altura, setAltura] = useState('');
+    const [resultadoImc, setResultadoImc] = useState('Vamos calcular seu IMC!') 
+    const [imc, setImc] = useState('0.00')
+
+    function calculaImc(e){
+
+        e.preventDefault();
+       
+        let imc = (peso/(altura*altura)).toFixed(2)
+
+        if(imc > 18.5 && imc < 25){
+            setResultadoImc('Você está no peso ideal, parabéns!')
+        }
+        else if(imc >= 25 && imc < 30){
+            setResultadoImc('Você está acima do peso, melhor fazer uma dieta!')
+        }
+        else if(imc >= 30){
+            setResultadoImc('Seu resultado indica obesidade. Procure um médico!')
+        }
+        else{
+            setResultadoImc('Você está abaixo do peso ideal. Isso não é bom!')
+        }
+
+        setImc(imc)
+        setPeso('')
+        setAltura('')
+    }
+
+    return(
+        <div className='container'>
+            <h1>Calcule seu IMC</h1>
+
+            <form className='form' onSubmit={calculaImc}>
+                <input 
+                    required
+                    type="number"R
+                    placeholder="Digite seu peso..." 
+                    value={peso}
+                    onChange={(e) => setPeso(e.target.value)} 
+                />
+                <input 
+                    required
+                    type='number'
+                    placeholder='Digite sua altura...'
+                    value={altura}
+                    onChange={(e) => setAltura(e.target.value)}
+                />
+
+                <button type='submit'>Calcular</button>
+            </form>
+
+            <div className='output'>
+                <div className='output-items'>
+                    <h2>{imc}</h2>
+                    <p>Seu IMC</p>
+                </div>
+                <div className='texto-imc'>
+                    <p>{resultadoImc}</p>
+                </div>
+            </div> 
+        </div>
+    )
+}
